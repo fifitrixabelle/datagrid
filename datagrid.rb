b9eb0342b89bc4f51f6ef795bbead3c7549fe83b
Felix Hawkins-Ozer <felix@hawkins-ozer.org>
2012-03-11 10:57:09 +0000

    add file loading

--- datagrid.rb
+++ datagrid.rb
@@ -1,5 +1,28 @@
+#!/usr/bin/env ruby
+
 require 'sinatra'
+require 'spreadsheet'
+require 'pry'
+
+$:.unshift File.join(".","lib")
+
+require 'base'
+
+# folders
+set :public_folder, File.dirname(__FILE__) + '/public'
+set :files_folder, File.dirname(__FILE__) + '/files'
+
+Spreadsheet.client_encoding = 'UTF-8'
 
 get '/' do
-    'Hello world!'
+  'Welcome to the reports'
+end
+
+get '/files/:year/:month/:report.:ext' do |year,month,report,ext|
+  path = File.join( settings.files_folder, year, month, report ) + "." + ext
+  if ext.downcase == "csv"
+    send_file path
+  else
+    book = Spreadsheet.open path
+  end
 end
